b70a4ee949684dbdcb95d02f6ec0dc04c4c1cfaa
Elias Mason <eliasjmason@gmail.com>
2016-08-20 18:59:35 -0700

    Adds JavaScript to handle basic open and close of prefs window.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,13 @@
+function handlePrefsModal() {
+    $('.btn-open-settings').on('click', function(){
+
+        $('.prefs').show();
+    });
+    $('.btn-close-settings').on('click', function(){
+        $('.prefs').hide();
+    });
+}
+
 $(document).ready(function(){
-    
+    handlePrefsModal();
 });
