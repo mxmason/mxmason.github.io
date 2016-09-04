bc3ef703f633bef71a24e0c37083a1d6ce8b3875
Elias Mason <eliasjmason@gmail.com>
2016-09-03 18:56:33 -0700

    Javascript now ensures the application of the user styles.

--- js/app.js
+++ js/app.js
@@ -1,47 +1,66 @@
-function populateStorage() {
-    var checkedContrastVal = (typeof $('input[name="high-contrast"]:checked').val() === 'undefined') ? "no" :  $('input[name="high-contrast"]:checked').val() ;
-    localStorage.setItem('isHighContrast', checkedContrastVal);
-
-    setPrefs();
-}
-
-function setPrefs() {
-    var contrastPref = localStorage.getItem('isHighContrast');
-
-    if (contrastPref === "yes") {
-        $('input[name="high-contrast"][value="yes"]').attr('checked', 'checked');
-        $('body').addClass('high-contrast');
-    } else {
-        $('input[name="high-contrast"][value="no"]').attr('checked', 'checked');
-        $('body').removeClass('high-contrast');
-    }
-}
-
-function handleStorage() {
-    if (!localStorage.getItem('isHighContrast')) {
+function checkStorage() {
+    if (!localStorage.getItem('contrast')) {
         populateStorage();
     } else {
-        setPrefs();
+        updateView();
     }
 }
 
-function handlePrefsModal() {
-    var $prefsModal = $('.prefs');
-    $('#prefs-btn').on('click', function(){
+function populateStorage(e) {
+    var prefName, prefVal;
+    // if there is an event from the view
+    if (e) {
+        prefName = $(e.target).attr('name');
+        prefVal = $(e.target).val();
+        //set the appropriate storage prop with data from the view
+        localStorage.setItem(prefName, prefVal);
+    }
+    updateView();
+}
+
+function updateView() {
+    var contrast = localStorage.getItem('contrast');
+    var userFont = localStorage.getItem('userFont');
+    switch (contrast) {
+        case "high":
+            $('.contrast-control[value="high"]').attr('checked', 'checked');
+            $('body').addClass('high-contrast');
+            break;
+        default:
+            $('.contrast-control[value="standard"]').attr('checked', 'checked');
+            $('body').removeClass('high-contrast');
+    }
+    switch (userFont) {
+        case "dyslexiaFriendly":
+            $('.font-control[value="dyslexiaFriendly"]').attr('checked', 'checked');
+            $('body').addClass('dyslexia-friendly');
+            break;
+        default:
+            $('.font-control[value="standard"]').attr('checked', 'checked');
+            $('body').removeClass('dyslexia-friendly');
+    }
+}
+
+$(document).ready(function() {
+    var $prefsLink = $('#openPrefs'),
+        $prefsModal = $('#prefsModal'),
+        $prefsForm = $('form'),
+        $prefsInputs = $('#prefsForm input');
+
+    $prefsLink.click(function() {
         $prefsModal.show();
     });
-    $('.btn-close-prefs').on('click', function(){
-        $prefsModal.hide();
-    });
-    $('form').on('submit', function(e){
+
+    $prefsForm.submit(function(e) {
         e.preventDefault();
-        populateStorage();
         $prefsModal.hide();
     });
-}
 
+    $prefsInputs.change(function(e) {
+        e.preventDefault();
+        populateStorage(e);
+    });
 
-$(document).ready(function(){
-    handleStorage();
-    handlePrefsModal();
+    checkStorage();
+
 });
