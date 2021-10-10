efae196eb9df9b884acb572e47909caf6fc02a36
EJ Mason <13525251+mxmason@users.noreply.github.com>
2021-10-10 12:51:33 -0700

    Prettify

--- .eleventy.js
+++ .eleventy.js
@@ -1,62 +1,62 @@
-let Nunjucks = require('nunjucks');
+let Nunjucks = require("nunjucks");
 
-const rssPlugin = require('@11ty/eleventy-plugin-rss');
-const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
-const markdownIt = require('markdown-it');
-const markdownItAnchor = require('markdown-it-anchor');
+const rssPlugin = require("@11ty/eleventy-plugin-rss");
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+const markdownIt = require("markdown-it");
+const markdownItAnchor = require("markdown-it-anchor");
 const htmlmin = require("html-minifier");
 
-const formatDate = require('./source/utils/format-date.js');
+const formatDate = require("./source/utils/format-date.js");
 
 module.exports = function (config) {
-  config.setFrontMatterParsingOptions({
-    summary: true,
-  });
+	config.setFrontMatterParsingOptions({
+		summary: true,
+	});
 
-  config.addLayoutAlias('base', 'source/layouts/base.njk');
-  config.addLayoutAlias('page', 'source/layouts/page.njk');
+	config.addLayoutAlias("base", "source/layouts/base.njk");
+	config.addLayoutAlias("page", "source/layouts/page.njk");
 
-  config.addPlugin(rssPlugin);
-  config.addPlugin(syntaxHighlight);
+	config.addPlugin(rssPlugin);
+	config.addPlugin(syntaxHighlight);
 
-  config.addPassthroughCopy('source/css/');
+	config.addPassthroughCopy("source/css/");
 
-  config.addFilter('formatDate', formatDate);
+	config.addFilter("formatDate", formatDate);
 
-  config.addTransform("htmlmin", function(content, outputPath) {
-    // Eleventy 1.0+: use this.inputPath and this.outputPath instead
-    if( outputPath && outputPath.endsWith(".html") ) {
-      let minified = htmlmin.minify(content, {
-        useShortDoctype: true,
-        removeComments: true,
-        collapseWhitespace: true
-      });
-      return minified;
-    }
+	config.addTransform("htmlmin", function (content, outputPath) {
+		// Eleventy 1.0+: use this.inputPath and this.outputPath instead
+		if (outputPath && outputPath.endsWith(".html")) {
+			let minified = htmlmin.minify(content, {
+				useShortDoctype: true,
+				removeComments: true,
+				collapseWhitespace: true,
+			});
+			return minified;
+		}
 
-    return content;
-  });
+		return content;
+	});
 
-  /* Markdown Plugins */
-  const options = {
-    html: true,
-    breaks: true,
-    linkify: true,
-  };
-  const opts = {
-    level: [1, 2, 3],
-    permalink: false,
-  };
-  config.setLibrary('md', markdownIt(options).use(markdownItAnchor, opts));
+	/* Markdown Plugins */
+	const options = {
+		html: true,
+		breaks: true,
+		linkify: true,
+	};
+	const opts = {
+		level: [1, 2, 3],
+		permalink: false,
+	};
+	config.setLibrary("md", markdownIt(options).use(markdownItAnchor, opts));
 
-  let nunjucksEnvironment = new Nunjucks.Environment(
-    new Nunjucks.FileSystemLoader(['source/layouts', 'source/assets'])
-  );
+	let nunjucksEnvironment = new Nunjucks.Environment(
+		new Nunjucks.FileSystemLoader(["source/layouts", "source/assets"])
+	);
 
-  config.setLibrary('njk', nunjucksEnvironment);
+	config.setLibrary("njk", nunjucksEnvironment);
 
-	const contentPath = '/content';
-	const nestedIndexPath = contentPath + '/index';
+	const contentPath = "/content";
+	const nestedIndexPath = contentPath + "/index";
 
 	config.addFilter("dropNestedContentPaths", function (path) {
 		if (path.indexOf(nestedIndexPath) === 0) {
@@ -67,16 +67,16 @@ module.exports = function (config) {
 			return path.slice(contentPath.length);
 		}
 
-		return path
+		return path;
 	});
 
-  return {
-    dir: {
-      input: 'source',
-      output: 'publish',
-      includes: 'assets',
-      layouts: 'layouts',
-    },
-    // templateFormats: ['njk', 'md'],
-  };
+	return {
+		dir: {
+			input: "source",
+			output: "publish",
+			includes: "assets",
+			layouts: "layouts",
+		},
+		// templateFormats: ['njk', 'md'],
+	};
 };
